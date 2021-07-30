062c8dfa1ed327ec38c2c80be15e8679ba994365
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-07-30 13:09:00 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+import os
+
+parser = argparse.ArgumentParser()
+# Execution selection
+parser.add_argument('--version_select', default='', type=str)
+parser.add_argument('--core_number', default='1', type=str)
+
+args = parser.parse_args()
+ver = args.version_select
+core = args.core_number
+c = int(core)
+
+if (ver == "naive" or "parallel" or "vectorized" or "multiprocessing"):
+  cwd = os.getcwd()
+  cwd = cwd + "/" + Implementations
+  os.chdir(cwd)
+  file_to_open = ver + ".py"
+  exec(open(file_to_open).read())
+elif ver == "test":
+  cwd = os.getcwd()
+  cwd = cwd + "/" + ver
+  os.chdir(cwd)
+  os.system("python -m unittest -v")
+else:
+  print("Wrong input. \n ver_select (naive or parallel or vectorized or multiprocessing or test) \n core_number: 1 to 32")
