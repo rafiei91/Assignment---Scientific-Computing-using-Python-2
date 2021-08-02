3365b4c32e04efec4d39fb43abd9747410ffdf19
rafiei91 <77726877+rafiei91@users.noreply.github.com>
2021-08-02 13:28:47 +0200

    Update parallel.py

--- Implementations/parallel.py
+++ Implementations/parallel.py
@@ -3,7 +3,7 @@ import time
 import sys
 sys.path.append('../')
 import os
-from code.funcs import Mf_p
+from code_f.funcs import Mf_p
 import h5py
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
